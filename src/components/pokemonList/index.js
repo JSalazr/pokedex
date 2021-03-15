abb1ebb4718ba52cb7919766da88df55915f31e7
JSalazr <jonathansalazar96@gmail.com>
2021-03-14 19:41:05 -0600

    Add rough pagination.

--- src/components/pokemonList/index.js
+++ src/components/pokemonList/index.js
@@ -1,23 +1,32 @@
 import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import Pagination from "@material-ui/lab/Pagination";
 import axios from "axios";
 import PokemonListItem from "./components/pokemonListItem";
 import useStyles from "./styles";
 
 const PokemonList = () => {
   const [pokemonList, setPokemonList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const pageSize = 21;
 
   const styles = useStyles();
 
   useEffect(() => {
-    if (pokemonList.length === 0) {
-      axios
-        .get("https://pokeapi.co/api/v2/pokemon?limit=151")
-        .then((response) => setPokemonList(response.data.results))
-        .catch((error) => console.log(error));
-    }
-  });
+    console.log("NO SE", currentPage);
+    axios
+      .get(
+        `https://pokeapi.co/api/v2/pokemon?limit=${pageSize}&offset=${currentPage*pageSize}`
+      )
+      .then((response) => {console.log(response); setPokemonList(response.data.results)})
+      .catch((error) => console.log(error));
+  }, [currentPage]);
+
+  const handlePagination = (e, newPage) => {
+    setCurrentPage(newPage - 1);
+  };
 
   return (
     <Grid item xs={12}>
@@ -27,6 +36,7 @@ const PokemonList = () => {
             <PokemonListItem dataUrl={pokemon.url} key={key} />
           ))}
         </Grid>
+        <Pagination onChange={handlePagination} count={898 / pageSize} />
       </Paper>
     </Grid>
   );
--- src/components/pokemonList/styles.js
+++ src/components/pokemonList/styles.js
@@ -2,8 +2,23 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    padding: "10px"
+    padding: "10px 25px",
+    position: "relative",
+    height: "65vh",
   },
+  navButton: {
+    borderRadius: "25px",
+    zIndex: 9,
+    top: "50%",
+    backgroundColor: "gray",
+    float: "left"
+  },
+  nextButton: {
+    left: "100%"
+  },
+  prevButton: {
+    left: "-5%"
+  }
 }));
 
 export default useStyles;
